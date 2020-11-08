148aa44692414a9e24b8de2804b7a85116b3abf4
Jun Villa <junarvi@yahoo.ca>
2020-11-08 08:43:02 -0500

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace bmviewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openButton = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.renderButton = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.skControl = new SkiaSharp.Views.Desktop.SKControl();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 12);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(86, 23);
+            this.openButton.TabIndex = 1;
+            this.openButton.Text = "Open...";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 78);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(86, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "time (ms)";
+            // 
+            // renderButton
+            // 
+            this.renderButton.Location = new System.Drawing.Point(12, 104);
+            this.renderButton.Name = "renderButton";
+            this.renderButton.Size = new System.Drawing.Size(86, 23);
+            this.renderButton.TabIndex = 5;
+            this.renderButton.Text = "Render";
+            this.renderButton.UseVisualStyleBackColor = true;
+            this.renderButton.Click += new System.EventHandler(this.renderButton_Click);
+            // 
+            // skControl
+            // 
+            this.skControl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.skControl.Location = new System.Drawing.Point(104, 11);
+            this.skControl.Name = "skControl";
+            this.skControl.Size = new System.Drawing.Size(800, 600);
+            this.skControl.TabIndex = 6;
+            this.skControl.Text = "skControl";
+            this.skControl.PaintSurface += new System.EventHandler<SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs>(this.skControl_PaintSurface);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 623);
+            this.Controls.Add(this.skControl);
+            this.Controls.Add(this.renderButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "bmviewer";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button renderButton;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private SkiaSharp.Views.Desktop.SKControl skControl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bmviewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "beatmap (*.osu)|*.osu|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Title = "Select beatmap";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        private void renderButton_Click(object sender, EventArgs e)
+        {
+            skControl.Invalidate();
+        }
+
+        private void skControl_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
+        {
+            Console.WriteLine("redraw");
+            var surface = e.Surface;
+            var width = e.Info.Width;
+            var height = e.Info.Height;
+            var canvas = surface.Canvas;
+
+            DrawGame(canvas, width, height);
+            canvas.Flush();
+        }
+
+        private void DrawGame(SKCanvas canvas, int width, int height)
+        {
+            // draw on the canvas
+            var paint1 = new SKPaint
+            {
+                IsAntialias = true,
+                Color = SKColors.Blue,
+                StrokeWidth = 15
+            };
+            canvas.Clear(SKColors.Black);
+            canvas.DrawCircle(50, 50, 30, paint1);
+        }
+    }
+}
